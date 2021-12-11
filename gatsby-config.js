f2a153356f963173b97a70a04c723c5331722f43
Dmitry Kraev <dmitry.kraev@samsungnext.com>
2021-12-11 09:59:36 +0300

    added gatsby cloud

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,6 +46,7 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sharp",
     "gatsby-plugin-image",
+    "gatsby-plugin-gatsby-cloud",
     {
       resolve: "gatsby-source-contentful",
       options: contentfulConfig,
